fbb73055e27b88773eda91772dd458f11c503918
dipesh <dipeshshrestha0314@gmail.com>
2024-02-23 18:38:35 -0500

    Initial Commit

---
+++ AccoBnB/AccoBnBApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccoBnBApp.swift
+//  AccoBnB
+//
+//  Created by Dipesh Shrestha on 2/23/24.
+//
+
+import SwiftUI
+
+@main
+struct AccoBnBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
