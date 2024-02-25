0eb3377443af2e23f16f953a32e5d73deab35060
Ayam <ayampant72@gmail.com>
2024-02-25 18:33:33 -0500

    splash screen view added

---
+++ AccoBnB/Components/SplashScreenView.swift
@@ -0,0 +1,50 @@
+//
+//  SplashScreenView.swift
+//  AccoBnB
+//
+//  Created by AP on 25/02/2024.
+//
+
+import SwiftUI
+
+struct SplashScreenView: View {
+    @State private var isSplashScreenActive = false
+    @State private var size = 0.8
+    @State private var opacity = 0.5
+    var body: some View {
+        if isSplashScreenActive {
+            ContentView()
+        } else {
+            VStack {
+                VStack {
+                    Image("ic_logo")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                    Text("Your Accomodation, Your Way")
+                        .font(.system(size: 28))
+                        .foregroundColor(Color("primaryColor"))
+                }
+                .scaleEffect(size)
+                .opacity(opacity)
+                .onAppear{
+                    withAnimation(.easeIn(duration: 1.5)){
+                        self.size = 0.9
+                        self.opacity = 1.0
+                    }
+                }
+            }
+            .onAppear{
+                DispatchQueue.main.asyncAfter(deadline: .now() + 2.4){ // change this timing to value after data from db is loaded.
+                    withAnimation{
+                        self.isSplashScreenActive = true
+                    }
+                }
+            }
+        }
+        
+    }
+}
+
+#Preview {
+    SplashScreenView()
+}
