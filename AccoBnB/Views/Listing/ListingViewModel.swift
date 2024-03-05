88995dbcb28281e2b6958db1d80c9c6f575e535e
Ayam <ayampant72@gmail.com>
2024-03-05 17:23:16 -0500

    Listing view, added viewmodel

---
+++ AccoBnB/Views/Listing/ListingViewModel.swift
@@ -0,0 +1,36 @@
+//
+//  ListingViewModel.swift
+//  AccoBnB
+//
+//  Created by AP on 05/03/2024.
+//
+
+import Foundation
+
+final class ListingViewModel : ObservableObject {
+    @Published var listings: [Listing] = []
+    @Published var isLoading = false // Track loading state
+    private let listingRepository: ListingRepository
+    
+    init(listingRepository: ListingRepository = FirestoreListingRepository()) {
+        self.listingRepository = listingRepository
+    }
+    
+    func getAllListings() {
+        isLoading = true // Show loader
+        listingRepository.getAllListings { [weak self] result in
+            self?.isLoading = false // Hide loader after operation completes
+            switch result {
+            case .success(let listings):
+                DispatchQueue.main.async {
+                    self?.listings = listings
+                    print("All listings loaded:", listings)
+                }
+            case .failure(let error):
+                print("Failed to fetch listings: \(error)")
+                // Handle error, such as showing an alert to the user
+            }
+        }
+    }
+}
+
