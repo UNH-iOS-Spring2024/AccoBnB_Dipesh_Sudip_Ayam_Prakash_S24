a5a1ff856e5bdafb49b0f7dabe3e77aea950a75c
Ayam <ayampant72@gmail.com>
2024-03-10 16:08:27 -0400

    merge conflict

--- AccoBnB/AccoBnBApp.swift
+++ AccoBnB/AccoBnBApp.swift
@@ -6,10 +6,16 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct AccoBnBApp: App {
     @StateObject var userStateVM = UserStateViewModel()
+    init(){
+        let providerFactory = AppCheckDebugProviderFactory()
+        AppCheck.setAppCheckProviderFactory(providerFactory)
+        FirebaseApp.configure()
+    }
     var body: some Scene {
         WindowGroup {
             SplashScreenView()
