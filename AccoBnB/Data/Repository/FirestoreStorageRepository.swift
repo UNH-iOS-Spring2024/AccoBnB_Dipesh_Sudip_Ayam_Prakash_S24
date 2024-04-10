0ee272e500946922178d3067df10fcb0c0e49874
Ayam <ayampant72@gmail.com>
2024-04-10 13:57:41 -0400

    seperate repo added to handle firebase storage related work

---
+++ AccoBnB/Data/Repository/FirestoreStorageRepository.swift
@@ -0,0 +1,40 @@
+//
+//  FirestoreStorageRepository.swift
+//  AccoBnB
+//
+//  Created by AP on 10/04/2024.
+//
+
+import Foundation
+
+import FirebaseStorage
+import UIKit
+
+class FirestoreStorageRepository : StorageRepository {
+    private let storage = Storage.storage()
+    
+    func uploadImagetoFirebaseStorage(_ image: UIImage,storageName: String, completion: @escaping (Result<String, any Error>) -> Void) {
+        guard let imageData = image.jpegData(compressionQuality: 0.5) else {
+            completion(.failure("Failed to convert image to data" as! Error))
+            return
+        }
+        
+        let storageRef = storage.reference()
+        let listingImagesRef = storageRef.child(storageName).child(UUID().uuidString)
+        let metadata = StorageMetadata()
+        metadata.contentType = "image/jpeg"
+        listingImagesRef.putData(imageData, metadata: metadata) { metadata, error in
+            if let error = error {
+                completion(.failure(error))
+            } else {
+                listingImagesRef.downloadURL { url, error in
+                    if let error = error {
+                        completion(.failure(error))
+                    } else if let downloadURL = url {
+                        completion(.success(downloadURL.absoluteString))
+                    }
+                }
+            }
+        }
+    }
+}
