711a66e7375ed67171d5ab2b53936981a5b7358f
dipesh <dipeshshrestha0314@gmail.com>
2024-04-11 11:35:55 -0400

    Added Map View, user location and markers for listing

--- AccoBnB/Components/MapView.swift
+++ AccoBnB/Components/MapView.swift
@@ -6,10 +6,37 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct MapView: View {
+    
+    @State var listings: [Listing] = []
+    @EnvironmentObject var listingViewModel: ListingViewModel
+    
+    @State var updatedListings: [MapAnnotationItems] = []
+    
+    
     var body: some View {
-        Text("Map View")
+        
+        Map() {
+            ForEach(updatedListings, id: \.id) { listing in
+                Marker(listing.text, coordinate: listing.coordinate)
+            }
+            UserAnnotation()
+        }
+        .mapControls {
+            MapUserLocationButton()
+            MapCompass()
+            MapScaleView()
+            MapPitchToggle()
+        }
+        .onAppear {
+            listings = listingViewModel.listings
+            updatedListings = listings.map { listing in
+                MapAnnotationItems(text: listing.title, lat: Double(listing.geoLocation!.lat) ?? 0.0, long: Double(listing.geoLocation!.long) ?? 0.0)
+                
+            }
+        }
     }
 }
 
---
+++ AccoBnB/Data/Model/MapAnnotationItems.swift
@@ -0,0 +1,20 @@
+//
+//  MapAnnotationItems.swift
+//  AccoBnB
+//
+//  Created by Dipesh Shrestha on 4/10/24.
+//
+
+import Foundation
+import MapKit
+
+struct MapAnnotationItems: Identifiable {
+    let id = UUID()
+    let text: String
+    let lat: Double
+    let long: Double
+    
+    var coordinate: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(latitude: lat, longitude: long)
+    }
+}
