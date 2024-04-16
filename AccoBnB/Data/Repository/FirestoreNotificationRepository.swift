1cb4c26ed0241e16ffad7e57c7dd9eda300c6fb2
sudeptech <118206782+sudeptech@users.noreply.github.com>
2024-04-16 13:47:01 -0400

    local notification handled

--- AccoBnB/Data/Repository/FirestoreNotificationRepository.swift
+++ AccoBnB/Data/Repository/FirestoreNotificationRepository.swift
@@ -15,6 +15,7 @@ class FirestoreNotificationRepository: NotificationRepository {
     func getNotifications(for userId: String, completion: @escaping (Result<[Notification], Error>) -> Void) {
         db.collection(notificaitonsCollection)
             .whereField("receiverId", isEqualTo: userId)
+            .order(by: "createdAt", descending: true)
             .addSnapshotListener{ snapshot, err in
                 if let error = err{
                     completion(.failure(error))
@@ -32,6 +33,22 @@ class FirestoreNotificationRepository: NotificationRepository {
                     }
                     switch result{
                     case .success(let notification):
+                        print(notification.createdAt!.timeIntervalSinceNow)
+                        if (notification.createdAt!.timeIntervalSinceNow > -5){
+                            let content = UNMutableNotificationContent()
+                            content.title = notification.title
+                            content.subtitle = notification.message
+                            content.sound = UNNotificationSound.default
+
+                            // show this notification five seconds from now
+                            let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+
+                            // choose a random identifier
+                            let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+
+                            // add our notification request
+                            UNUserNotificationCenter.current().add(request)
+                        }
                         return notification
                     case .failure(let error):
                         print("Error in decoding notifications \(error)")
